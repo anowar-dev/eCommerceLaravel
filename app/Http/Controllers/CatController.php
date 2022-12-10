5178037150a9680f20b987710c5f93ed2c062f1e
anowar-dev <anowerhossan1988@gmail.com>
2022-12-10 23:54:02 +0600

    First commit for this development

---
+++ app/Http/Controllers/CatController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatController extends Controller
+{
+    //
+    function show(){
+        // echo "cat show";
+        return view('admin.cat');
+    }
+    function insert(Request $req){
+        $cat_name = $req->cat_name;
+        return view('admin.cat', compact('cat_name'));
+    }
+}
